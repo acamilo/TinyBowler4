acb48414a9651298e74520af4660c521d171a1fc
acamilo <alex.camilo@gmail.com>
2013-02-21 23:16:22 -0500

    changed names of hook macros

--- bowler.h
+++ bowler.h
@@ -18,30 +18,30 @@ typedef struct {
 void printParserState(V4MicroParser_state *s);
 char runParserSM(V4MicroParser_state *s);
 
-
+#define DEBUG
 /* SM Hooks */
 #ifdef DEBUG
 
-	#define SM_ALIGN(s)			printf("V4MicroParser[%X]: State: ALIGN \n",s);
-	#define SM_ALIGN_ENOUGHBYTES(s)		printf("V4MicroParser[%X]:\tHave %d bytes Attempting to parse\n",s,sizeof(bowler4_header_fields));
-	#define SM_ALIGN_XSUMMATCH(s)		printf("V4MicroParser[%X]:\tValid Packet!\n",s);
-	#define SM_ALIGN_COURRUPTDATA(s,b)	printf("V4MicroParser[%X]:\tPacket is not valid. Discarded %d bytes\n",s,b);
+	#define HOOK_SM_ALIGN(s)			printf("V4MicroParser[%X]: State: ALIGN \n",s);
+	#define HOOK_SM_ALIGN_ENOUGHBYTES(s)		printf("V4MicroParser[%X]:\tHave %d bytes Attempting to parse\n",s,sizeof(bowler4_header_fields));
+	#define HOOK_SM_ALIGN_XSUMMATCH(s)		printf("V4MicroParser[%X]:\tValid Packet!\n",s);
+	#define HOOK_SM_ALIGN_COURRUPTDATA(s,b)		printf("V4MicroParser[%X]:\tPacket is not valid. Discarded %d bytes\n",s,b);
 
-	#define SM_PAYLOADWAIT(s)
+	#define HOOK_SM_PAYLOADWAIT(s)
 
-	#define SM_PAYLOADWAIT(s)
+	#define HOOK_SM_PAYLOADWAIT(s)
 
 
 #else
 
-	#define SM_ALIGN(s)
-	#define SM_ALIGN_ENOUGHBYTES(s)
-	#define SM_ALIGN_XSUMMATCH(s)
-	#define SM_ALIGN_COURRUPTDATA(s,b)
+	#define HOOK_SM_ALIGN(s)
+	#define HOOK_SM_ALIGN_ENOUGHBYTES(s)
+	#define HOOK_SM_ALIGN_XSUMMATCH(s)
+	#define HOOK_SM_ALIGN_COURRUPTDATA(s,b)
 
-	#define SM_PAYLOADWAIT(s)
+	#define HOOK_SM_PAYLOADWAIT(s)
 
-	#define SM_PAYLOADWAIT(s)
+	#define HOOK_SM_PAYLOADWAIT(s)
 #endif
 
 #endif
